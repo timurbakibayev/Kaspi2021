593943849e65790e23fd6694bb0d4ea868bba620
Timur Bakibayev <timurbakibayev@gmail.com>
2021-03-16 12:11:36 +0100

    Complex chess

--- chess.py
+++ chess.py
@@ -6,3 +6,6 @@ def p_allowed(cell_from,cell_to,white):
     else:
         return x1 == x2 and y1 - 1 == y2
 
+
+def board_move_ok(cell_from, cell_to, board):
+    return True
---
+++ tests_complex_chess.py
@@ -0,0 +1,26 @@
+import unittest
+from chess import *
+
+class TestChessMethods(unittest.TestCase):
+    def test_complex_ok(self):
+        board = [
+            ["BR", "BN", "BB", "BQ", "BK", "BB", "BN", "  "],
+            ["BP", "BP", "BP", "  ", "BP", "BP", "  ", "  "],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "BP", "  "],
+            ["  ", "  ", "  ", "BP", "  ", "  ", "  ", "BR"],
+            ["  ", "  ", "  ", "WP", "WP", "  ", "WP", "BP"],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+            ["WP", "WP", "WP", "  ", "  ", "WP", "  ", "WP"],
+            ["WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR"],
+        ]
+
+        self.assertTrue(board_move_ok((1,2), (1,4), board))  # WP
+        self.assertTrue(board_move_ok((1,2), (1,3), board))
+        self.assertTrue(board_move_ok((5,4), (4,5), board))  # EAT
+
+        self.assertFalse(board_move_ok((1,2), (2,2), board))
+        self.assertFalse(board_move_ok((1,2), (2,3), board))
+
+
+if __name__ == '__main__':
+    unittest.main()
